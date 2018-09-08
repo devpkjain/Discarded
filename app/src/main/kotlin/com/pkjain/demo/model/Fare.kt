f22db31b4ce468b9f1f826e6eb090b20932f96a4
Pankaj Jain - vn0rihl <Pankaj.Jain@walmartlabs.com>
2018-09-08 09:31:36 -0700

    model

--- app/src/main/kotlin/com/pkjain/demo/model/Fare.kt
+++ app/src/main/kotlin/com/pkjain/demo/model/Fare.kt
@@ -3,8 +3,8 @@ package com.pkjain.demo.model
 /**
  *This class defines fare info
  */
-data class FareInfo(val Adult: Fares, val Child: Fares, val Senior: Fares)
+data class Rider(val Adult: Fares, val Child: Fares, val Senior: Fares)
 
-data class Fares(val fares: List<Fare>)
+data class Fares(val fares: List<Fare>, val subText: String?)
 
 data class Fare(val description: String, val price: Float)
